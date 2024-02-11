1fd2696f07748cc17c809490b24c0bc1cef32286
Eduardo Silveira <onlydatagc@gmail.com>
2024-02-11 18:30:26 -0300

    Versão inicial com esqueleto do layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { FieldsetModule } from 'primeng/fieldset';
+import { InputMaskModule } from 'primeng/inputmask';
+import { CalendarModule } from 'primeng/calendar';
+import { TableModule } from 'primeng/table';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    InputTextModule,
+    FieldsetModule,
+    InputMaskModule,
+    CalendarModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
